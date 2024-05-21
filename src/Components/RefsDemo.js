9eb7f9d05e95cd654c986de00737af1d485799e3
Prit <prit sakhvala@king.com>
2024-05-21 10:23:42 +0530

    create ref using createref()

---
+++ src/Components/RefsDemo.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class RefsDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  componentDidMount() {
+    this.myRef.current.focus;
+    console.log(this.myRef);
+  }
+  handleEvent = () => {
+    alert(this.myRef.current.value);
+  };
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.myRef} />
+        <button onClick={this.handleEvent}>submit</button>
+      </div>
+    );
+  }
+}
+
+export default RefsDemo;
